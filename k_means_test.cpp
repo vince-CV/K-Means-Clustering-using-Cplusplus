4925af5e3fbd796caa3d2a02a3ae6ba5ecc7305a
Xunzhe Wen <xwen258@gmail.com>
2019-02-20 15:23:30 -0500

    main function for test

---
+++ k_means_test.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include "json11.hpp"
+#include "vinceAlgorithm.h"
+
+
+using namespace cv;
+using namespace std;
+using namespace vince;
+
+int main() 
+{
+
+	vector<Point2f> projectdata = getROIs(project);
+	vector<Point2f> kmeans_centroids;
+
+	kmeans_centroids = K_Means_Centroids(projectdata, 12);
+
+	return 0;
+
+}
+
+
+
